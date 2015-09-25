23b5220bce682d056ebee1e91d1a2e38fc26de79
pli888 <peter@gigasciencejournal.com>
2015-09-25 10:27:52 +0800

    Delete use of app-view.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var app = app || {};
 
 $(function () {
     // Kick things off by creating the **App**
-    new app.AppView();
+    //new app.AppView();
     // We create a CytoscapeWorkflow model here which is used for the whole
     // web application
     //var cy_wf = new app.CytoscapeWorkflow();
