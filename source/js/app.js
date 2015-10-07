57b704e14a29328cbe28455ac005630a38ced917
Peter Li <peter@gigasciencejournal.com>
2015-10-07 15:27:28 +0100

    Display workflow using dagre layout

--- source/js/app.js
+++ source/js/app.js
@@ -5,21 +5,32 @@
 
 var app = angular.module('app', ['ui.bootstrap', 'flow']);
 
-// use a factory instead of a directive, because cy.js is not just for visualisation; you need access to the graph model and events etc 
-app.factory('peopleGraph', ['$q', function ($q) {
+// use a factory instead of a directive, because cy.js is not just for visualisation; you need access to the graph model and events etc
+app.factory('workflowGraph', ['$q', function ($q) {
     var cy;
-    var peopleGraph = function (people) {
+    var workflowGraph = function (nodes, edges) {
         var deferred = $q.defer();
 
-        // put people model in cy.js
+        // put nodes and edges model in cy.js
         var eles = [];
-        for (var i = 0; i < people.length; i++) {
+        for (var i = 0; i < nodes.length; i++) {
             eles.push({
                 group: 'nodes',
                 data: {
-                    id: people[i].id,
-                    weight: people[i].weight,
-                    name: people[i].name
+                    id: nodes[i].id,
+                    name: nodes[i].name,
+                    color: nodes[i].color
+                }
+            });
+        }
+        for (i = 0; i < edges.length; i++) {
+            eles.push({
+                group: 'edges',
+                data: {
+                    id: edges[i].id,
+                    name: edges[i].weight,
+                    source: edges[i].source,
+                    target: edges[i].target
                 }
             });
         }
@@ -33,29 +44,42 @@ app.factory('peopleGraph', ['$q', function ($q) {
                     .selector('node')
                     .css({
                         'content': 'data(name)',
-                        'height': 80,
-                        'width': 'mapData(weight, 1, 200, 1, 200)',
-                        'text-valign': 'center',
-                        'color': 'white',
-                        'text-outline-width': 2,
-                        'text-outline-color': '#888'
+                        'shape': 'circle',
+                        'background-color': 'data(color)',
+                        'font-size': 10,
+                        'text-valign': 'bottom',
+                        'color': '#6e6e6e'
                     })
                     .selector('edge')
                     .css({
-                        'target-arrow-shape': 'triangle'
+                        'target-arrow-shape': 'triangle',
+                        'width': 2,
+                        'line-color': '#bfbfbf',
+                        'target-arrow-color': '#bfbfbf'
                     })
-                    .selector(':selected')
+
+                    // When selected using a dragged box
+                    .selector(":selected")
                     .css({
-                        'background-color': 'black',
-                        'line-color': 'black',
-                        'target-arrow-color': 'black',
-                        'source-arrow-color': 'black',
-                        'text-outline-color': 'black'
+                        "line-color": "#636363",
+                        "target-arrow-color": "#636363",
+                        "background-blacken": "0.3"
+                    })
+
+                    .selector('.highlighted')
+                    .css({
+                        'background-color': '#61bffc',
+                        'line-color': '#61bffc',
+                        'target-arrow-color': '#61bffc',
+                        'transition-property': 'background-color, line-color, target-arrow-color',
+                        'transition-duration': '0.5s'
                     }),
 
                 layout: {
-                    name: 'cose',
-                    padding: 10
+                    name: 'dagre',
+                    directed: true,
+                    padding: 10,
+                    rankDir: 'TB'
                 },
 
                 elements: eles,
@@ -63,15 +87,15 @@ app.factory('peopleGraph', ['$q', function ($q) {
                 ready: function () {
                     deferred.resolve(this);
 
-                    cy.on('cxtdrag', 'node', function (e) {
-                        var node = this;
-                        var dy = Math.abs(e.cyPosition.x - node.position().x);
-                        var weight = Math.round(dy * 2);
-
-                        node.data('weight', weight);
-
-                        fire('onWeightChange', [node.id(), node.data('weight')]);
-                    });
+                    //cy.on('cxtdrag', 'node', function (e) {
+                    //    var node = this;
+                    //    var dy = Math.abs(e.cyPosition.x - node.position().x);
+                    //    var weight = Math.round(dy * 2);
+                    //
+                    //    node.data('weight', weight);
+                    //
+                    //    fire('onWeightChange', [node.id(), node.data('weight')]);
+                    //});
                 }
             });
 
@@ -80,10 +104,10 @@ app.factory('peopleGraph', ['$q', function ($q) {
         return deferred.promise;
     };
 
-    peopleGraph.listeners = {};
+    workflowGraph.listeners = {};
 
     function fire(e, args) {
-        var listeners = peopleGraph.listeners[e];
+        var listeners = workflowGraph.listeners[e];
 
         for (var i = 0; listeners && i < listeners.length; i++) {
             var fn = listeners[i];
@@ -93,66 +117,72 @@ app.factory('peopleGraph', ['$q', function ($q) {
     }
 
     function listen(e, fn) {
-        var listeners = peopleGraph.listeners[e] = peopleGraph.listeners[e] || [];
+        var listeners = workflowGraph.listeners[e] = workflowGraph.listeners[e] || [];
 
         listeners.push(fn);
     }
 
-    peopleGraph.setPersonWeight = function (id, weight) {
+    workflowGraph.setPersonWeight = function (id, weight) {
         cy.$('#' + id).data('weight', weight);
     };
 
-    peopleGraph.onWeightChange = function (fn) {
+    workflowGraph.onWeightChange = function (fn) {
         listen('onWeightChange', fn);
     };
 
-    return peopleGraph;
-
-
+    return workflowGraph;
 }]);
 
-app.controller('PeopleCtrl', ['$scope', 'peopleGraph', function ($scope, peopleGraph) {
+
+app.controller('NavBarCtrl', ['$scope', 'workflowGraph', function ($scope, workflowGraph) {
+
     var cy; // maybe you want a ref to cy
     // (usually better to have the srv as intermediary)
 
-    $scope.people = [
-        {id: 'l', name: 'Laurel', weight: 65},
-        {id: 'h', name: 'Hardy', weight: 110}
+    $scope.nodes = [
+        {id: 'a', name: 'Select populations', color: '#D9EDF7'},
+        {id: 'b', name: 'Count', color: '#D9EDF7'},
+        {id: 'c', name: 'Filter', color: '#D9EDF7'},
+        {id: 'd', name: 'Select first', color: '#D9EDF7'},
+        {id: 'e', name: 'Remove beginning', color: '#D9EDF7'},
+        {id: 'f', name: 'Sort', color: '#D9EDF7'},
+        {id: 'g', name: 'Concatenate datasets', color: '#D9EDF7'},
+        {id: 'h', name: 'SmileFinder', color: '#D9EDF7'},
+        {id: 'i', name: 'Grapher', color: '#D9EDF7'},
+        {id: 'j', name: 'graph', color: '#FCF8E3'}
     ];
 
-    var peopleById = {};
-    for (var i = 0; i < $scope.people.length; i++) {
-        var p = $scope.people[i];
+    $scope.edges = [
+        {id: 'ab', weight: 1, source: 'a', target: 'b'}, //Select populations > Count
+        {id: 'bc', weight: 2, source: 'b', target: 'c'}, //Count > Filter
+        {id: 'cd', weight: 3, source: 'c', target: 'd'}, //Filter > Select first
+        {id: 'ce', weight: 4, source: 'c', target: 'e'}, //Filter > Remove beginning
+        {id: 'ef', weight: 5, source: 'e', target: 'f'}, //Remove beginning > Sort
+        {id: 'dg', weight: 6, source: 'd', target: 'g'}, //Select First > Concatenate datasets
+        {id: 'fg', weight: 6, source: 'f', target: 'g'}, //Sort > Concatenate datasets
+        {id: 'gh', weight: 7, source: 'g', target: 'h'}, //Concatenate datasets > SmileFinder
+        {id: 'hi', weight: 8, source: 'h', target: 'i'}, //SmileFinder > Grapher
+        {id: 'ij', weight: 8, source: 'i', target: 'j'}  //Grapher > Graph
 
-        peopleById[p.id] = p;
-    }
+    ];
 
     // you would probably want some ui to prevent use of PeopleCtrl until cy is loaded
-    peopleGraph($scope.people).then(function (peopleCy) {
-        cy = peopleCy;
+    workflowGraph($scope.nodes, $scope.edges).then(function (workflowCy) {
+        cy = workflowCy;
 
         // use this variable to hide ui until cy loaded if you want
         $scope.cyLoaded = true;
     });
 
-    $scope.onWeightChange = function (person) {
-        peopleGraph.setPersonWeight(person.id, person.weight);
-    };
+    //$scope.onWeightChange = function (person) {
+    //    workflowGraph.setPersonWeight(person.id, person.weight);
+    //};
 
-    peopleGraph.onWeightChange(function (id, weight) {
-        peopleById[id].weight = weight;
-
-        $scope.$apply();
-    });
-
-}]);
-
-app.controller('NavBarCtrl', function ($scope, $log) {
-    $scope.items = [
-        'The first choice!',
-        'And another choice for you.',
-        'but wait! A third!'
-    ];
+    //workflowGraph.onWeightChange(function (id, weight) {
+    //    peopleById[id].weight = weight;
+    //
+    //    $scope.$apply();
+    //});
 
     $scope.status = {
         isopen: false
@@ -223,6 +253,4 @@ app.controller('NavBarCtrl', function ($scope, $log) {
         console.log("LoadExample clicked!!");
     };
 
-
-
-});
+}]);
